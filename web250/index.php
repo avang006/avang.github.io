8193f44954bc0e40209f45df6b9014883477d4ad
alyssa vang <54256262+avang006@users.noreply.github.com>
2024-11-04 20:05:30 -0500

    Create index.php

---
+++ web250/index.php
@@ -0,0 +1,12 @@
+<?php
+$sOutputString = "<hr/>";
+
+
+for($iCounter = 1; $iCounter <= 1000; $iCounter++)
+{
+     $sOutputString .= $iCounter . ") alyssa got PHP running on my computer!! ";
+}
+
+
+echo ("<h5>" . $sOutputString . "</h5><hr/>");
+?>
