2494cb40a263fc48d0d73779d831189e9bdc1f0e
alyssa vang <54256262+avang006@users.noreply.github.com>
2024-11-05 23:28:20 -0500

    Create brand.php

---
+++ web250/brand.php
@@ -0,0 +1,56 @@
+<h2>Fuschia Dance Academy</h2>
+               <h3>Mission Statement:</h3>
+               <p class="about">At Fuchsia Dance Academy, our vision is to enable each individual to creatively express their multicultural backgrounds through dance. We are always eager to share these diverse cultures with our audiences. We are committed to providing the highest quality dance education and resources to help our dancers achieve their goals.</p>
+            <h3>
+               Color Scheme:
+            </h3>
+            <div class="boxb">
+               <p class="inside">#50258C</p>
+            </div>
+            <div class="boxc">
+               <p class="inside">
+                  #E4EBF7
+               </p>
+            </div>
+            <div class="boxd">
+               <p class="inside">
+                  #C412BD
+               </p>
+            </div>
+            <div class="boxe">
+               <p class="inside">
+                  #C69DEB
+               </p>
+            </div>
+            <table>
+               <tr>
+                  <td>Main Background &amp; Font color</td>
+                  <td>#50258C</td>
+               </tr>
+               <tr>
+                  <td>Link Hover Color</td>
+                  <td>#E4EBF7</td>
+               </tr>
+               <tr>
+                  <td>Link Background Hover Color</td>
+                  <td>#C412BD</td>
+               </tr>
+               <tr>
+                  <td>Div Background</td>
+                  <td>#C69DEB</td>
+               </tr>
+            </table>
+            <span class="bold">
+               <h3>Font:</h3>
+            </span>
+            <p class="fonta">
+            Sofia Font</p>
+            <p class="fonta">
+               Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz
+            </p>
+            <p class="fontb">
+               Trirong Font
+            </p>
+            <p class="fontb">
+               Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz
+            </p>
