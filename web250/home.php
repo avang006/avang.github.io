e5d869e1669b8c58a20065577c88376e7090d23c
alyssa vang <54256262+avang006@users.noreply.github.com>
2024-11-07 12:52:48 -0500

    Delete web250/home.php

--- web250/home.php
+++
@@ -1,6 +0,0 @@
-<h2>Home</h2>
-               <p>Welcome to my Database Driven Website home page!</p>
-               <p>Feel free to explore my webpage by navigating through my navigation links.</p>
-               <p><a href="demo.php" id="namelink">PHP</a></p>
-               <p><a href="https://avang.space/multipage_sites/superduper_php/index.php">superduper_php</a></p>
-               <p><a href="https://avang.space/multipage_sites/superduper_static/index.htm">superduper_static</a></p>
